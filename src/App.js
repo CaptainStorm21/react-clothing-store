e6f43941141244339955f52915282403dd1b6041
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-13 22:08:21 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+//router
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage =  () => (
+  <div>
+    <h1>HATS</h1>
+  </div>
+
+)
+
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* //exact will render only that component
+       <Route exact path='/' component = { HomePage }/>
+      //if you set exact = "false it will render homepage and 
+      //the current component together" */}
+      <Switch>
+        <Route exact path='/' component = { HomePage }/>  
+        <Route  path='/hats' component = { HatsPage }/>
+      </Switch>
+
     </div>
   );
 }
